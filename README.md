a3680350ccb13914c151997cd0f69af41c69af49
vyaskruti <vyaskruti89@gmail.com>
2019-10-23 19:31:16 -0400

    project1

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Hangman
 Project1 for javascript fundamental class fall 2019
+# workspace
+.project
+**/.vscode/*
+!**/.vscode/launch.json
+*.code*
+.idea
+.sublime*
+*.swp
---
+++ index.js
@@ -0,0 +1,6 @@
+const prompt = require('readline-sync');
+const wordBank = require('./word-bank.json');
+
+const myVariable = prompt.question("say somethingS");
+console.log(myVariable + "/n");
+console.log(myVariable);
